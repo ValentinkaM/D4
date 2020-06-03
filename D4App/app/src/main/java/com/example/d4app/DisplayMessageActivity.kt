aeda081e8a47beb63244cd1ada7aa3cbab8c797b
Jade Kelly <jade.jnrkelly@gmail.com>
2020-06-02 17:28:11 -0700

    fixed app crash issue - see 'Bug-Fix App Crashing' card in completed tasks for an overview of the problem and the changes made

--- D4App/app/src/main/java/com/example/d4app/DisplayMessageActivity.kt
+++ D4App/app/src/main/java/com/example/d4app/DisplayMessageActivity.kt
@@ -2,21 +2,11 @@ package com.example.d4app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)//FROM VALENTINA: it gives me the error I changed it from activiti_display_message to activity_main
-
-        // Get the Intent that started this activity and extract the string
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
-
-        // Capture the layout's TextView and set the string as its text
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
-        }
+        setContentView(R.layout.activity_display_message)
     }
 }
