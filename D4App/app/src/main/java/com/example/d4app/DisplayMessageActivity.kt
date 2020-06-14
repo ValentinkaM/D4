487b12a0232340ca38d7da685d8c489ae51c42e6
Tammie Turpen <Tammie.turpen@bellevuecollege.edu>
2020-06-13 18:16:49 -0700

    adding changes to save and retrieve profile to/from file

--- D4App/app/src/main/java/com/example/d4app/DisplayMessageActivity.kt
+++ D4App/app/src/main/java/com/example/d4app/DisplayMessageActivity.kt
@@ -2,11 +2,59 @@ package com.example.d4app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.InputStreamReader
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        val dataValues : String
+        val name = findViewById<EditText>(R.id.displayName)
+        val age = findViewById<EditText>(R.id.displayAge)
+        val height = findViewById<EditText>(R.id.displayHeight)
+        val weight = findViewById<EditText>(R.id.displayWeight)
+
+
+
+        val filename = "ProfileData.txt"
+            var fileInputStream: FileInputStream? = null
+            fileInputStream = openFileInput(filename)
+            var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder: StringBuilder = StringBuilder()
+            var text: String? = null
+            while ({ text = bufferedReader.readLine(); text }() != null) {
+                stringBuilder.append(text)
+            }
+
+        dataValues = stringBuilder.toString()
+
+        var result = dataValues.split(",").map { it.trim() }
+        result.forEach(fun(it: String) {
+            when {
+                it.startsWith("name:", true) -> {
+                    name.setText(it.substringAfter(":")).toString()
+                }
+                it.startsWith("birthdate:", true) -> {
+                    // TODO: Implement Age Calculation
+                    age.setText(it.substringAfter(":")).toString()
+                }
+                it.startsWith("heightin:", true) -> {
+                    height.setText(it.substringAfter(":")).toString()
+                }
+                it.startsWith("weight:", true) -> {
+                    weight.setText(it.substringAfter(":")).toString()
+                }
+            }
+
+
+        })
+
     }
 }
--- D4App/app/src/main/java/com/example/d4app/MainActivity.kt
+++ D4App/app/src/main/java/com/example/d4app/MainActivity.kt
@@ -1,15 +1,65 @@
 package com.example.d4app
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name = findViewById<EditText>(R.id.editPersonName)
+        val birthDate = findViewById<EditText>(R.id.editBirthdate)
+        val heightFeet = findViewById<EditText>(R.id.editHeightInFeet)
+        val heightInches = findViewById<EditText>(R.id.editHeightIn)
+        val weight = findViewById<EditText>(R.id.editWeight)
+
+        val btnSave = findViewById<Button>(R.id.buttonSave)
+       //  val btnView = findViewById<Button>(R.id.btnView)
+
+
+        btnSave.setOnClickListener(View.OnClickListener {
+            val file:String = "ProfileData.txt"
+            val data:String = "name:" + name.text.toString() + ",birthdate:" + birthDate.text.toString() + ",heightFeet:" + heightFeet.text.toString() + ",heightInches:" + heightInches.text.toString() + ",weight:" + weight.text.toString()
+                /*
+            val name:String = "name:" + name.text.toString()
+            val birthDate:String = "birthdate:" + birthDate.text.toString()
+            val heightFeet:String = "heightFeet:" + heightFeet.text.toString()
+            val heightInches:String = "heightInches:" + heightInches.text.toString()
+            val weight:String = "weight:" + weight.text.toString()
+            */
+
+            val fileOutputStream: FileOutputStream
+            try {
+                fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)
+                fileOutputStream.write(data.toByteArray())
+            } catch (e: FileNotFoundException){
+                e.printStackTrace()
+            }catch (e: NumberFormatException){
+                e.printStackTrace()
+            }catch (e: IOException){
+                e.printStackTrace()
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+            Toast.makeText(applicationContext,"data save",Toast.LENGTH_LONG).show()
+           // fileName.text.clear()
+           // fileData.text.clear()
+            val intent = Intent(this, DisplayMessageActivity::class.java)
+            startActivity(intent)
+        })
+
+
     }
     /** Called when the user taps the D4 Logo button */
     fun sendMessage(view: View) {
@@ -18,3 +68,4 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
